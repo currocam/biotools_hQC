8e99c1d19f339ddec2e7906138c8c2c0f1113e52
currocam <campuzanocurro@gmail.com>
2021-12-09 13:36:22 +0100

    feat: crear alinearZ

---
+++ alinear_Z/alinear_Z.lpr
@@ -0,0 +1,22 @@
+program alinear_Z;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, unit1_alinear_Z
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Scaled:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
---
+++ alinear_Z/unit1_alinear_z.pas
@@ -0,0 +1,37 @@
+unit unit1_alinear_Z;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Spin, biotools;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Image1: TImage;
+    Image2: TImage;
+    Memo1: TMemo;
+    SpinEdit1: TSpinEdit;
+    SpinEdit2: TSpinEdit;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
